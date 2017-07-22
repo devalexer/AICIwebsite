54fd454c5975a47ab77296cf4cbc542abdd10c1d
devalexer <alliemckay.dev@gmail.com>
2017-07-22 11:05:41 -0400

    Bootstrap theme applied (agency)

--- AICIwebsite/App_Start/BundleConfig.cs
+++ AICIwebsite/App_Start/BundleConfig.cs
@@ -9,7 +9,15 @@ namespace AICIwebsite
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/js/agency.js",
+                        "~/Scripts/js/agency.min.js",
+                        "~/Scripts/js/contact_me.js",
+                        "~/Scripts/jqBootstrapValidation.js",
+                        "~/Content/vendor/bootstrap/js/bootstrap.js",
+                        "~/Content/vendor/bootstrap/js/bootstrap.min.js",
+                        "~/Content/vendor/jquery/jquery.js",
+                        "~/Content/vendor/jquery/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,8 +32,15 @@ namespace AICIwebsite
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/agency.css",
+                      "~/Content/css/agency.min.css",
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/font-awesome/css/font-awesome.css",
+                      "~/Content/vendor/font-awesome/css/font-awesome.min.css"));
         }
     }
 }
